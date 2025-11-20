15485d505df36584b6c4eefe26373a9427b59c9d
Bayuu7 <103884982+Bayuu7@users.noreply.github.com>
2025-11-21 00:49:41 +0700

    Create Mesh.js

---
+++ src/core/Mesh.js
@@ -0,0 +1,12 @@
+export class Mesh {
+  constructor(){
+    this.vertices = [];
+    this.normals = [];
+    this.indices = [];
+    this.position = [0,0,0];
+  }
+
+  draw(gl, camera, lights){
+    // placeholder: nanti pakai WebGL shader dan buffer
+  }
+}
